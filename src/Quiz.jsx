287472a10eda5c95da919f9b427155cd7648618c
TheoThibodeau <teddythibodeau@gmail.com>
2023-06-14 20:57:19 -0400

    styling and functionality

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Category from './Category'
@@ -8,32 +8,46 @@ function Quiz({categoryID}) {
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const questionURL = `https://opentdb.com/api.php?amount=10&category=${categoryID}`
     const [color, setColor] = useState('')
+    const [score, setScore] = useState(0)
+    const [finalScore, setFinalScore] = useState()
 
     useEffect(() => {
         axios.get(questionURL).then((response) => setQuestions(response.data.results))}, [categoryID])
 
     const handleNextQuestion = () => {
       setCurrentQuestion(currentQuestion + 1)
+      setColor('')
     }
 
+    const handleAnswer = (answer) => {
+      if (answer === questions[currentQuestion].correct_answer) {
+        setColor('green');
+        setScore(score + 1)
+        // setFinalScore(finalScore + 1) doesn't do anything yet
+      } else {
+        setColor('red');
+      }
+    };
+
     const handleGoBack = () => {
-      setCurrentQuestion(Cateogry)
+      setCurrentQuestion(0)
     }
 
     console.log(questions)
     return (
       <>
       <h1>Quiz</h1>
-      <div className='slideContainer'>
+    <div className='slideContainer'>
       {questions.length > 0 ? (  
         <div className='questionSlides'>
           <h2 className='title'>{questions[currentQuestion].question}</h2>
           <div className='answerButtons'>
-            <button onClick={handleNextQuestion}>{questions[currentQuestion].correct_answer}</button>
-            <div className={color}>
-              <button onClick={() => setColor('red')}>{questions[currentQuestion].incorrect_answers[0]}</button>
-              <button onClick={() => setColor('yellow')}>{questions[currentQuestion].incorrect_answers[1]}</button>
-              <button onClick={() => setColor('red')}>{questions[currentQuestion].incorrect_answers[2]}</button>
+              <button className={color} onClick={() => handleAnswer(questions[currentQuestion].correct_answer)}>
+                {questions[currentQuestion].correct_answer}</button>
+            <div className='answerButtons'>
+              <button className={color} onClick={() => setColor('red')}>{questions[currentQuestion].incorrect_answers[0]}</button>
+              <button className={color} onClick={() => setColor('red')}>{questions[currentQuestion].incorrect_answers[1]}</button>
+              <button className={color} onClick={() => setColor('red')}>{questions[currentQuestion].incorrect_answers[2]}</button>
             </div>
           </div>
         </div>
@@ -41,14 +55,28 @@ function Quiz({categoryID}) {
       }
         <div>
           <button className="nextQuestion" onClick={handleNextQuestion}>Next Question</button>
-          <button className="goBack" onClick={handleGoBack}>Go Back</button>
         </div>
-        {questions[currentQuestion] ? 'Correct!' : 'Incorrect, the correct answer is'}
-
-      </div>
-
+        {color === 'green' ? ('Correct!') : color === 'red' ? (`Incorrect, the correct answer is: 
+        ${questions[currentQuestion].correct_answer}`) : ('')}
+        <p>Your Score: {score}/10</p>
+    </div>
+    {/* <div>
+      {currentQuestion === question.length[9] (
+        <div>Your Final Score: {finalScore}/10</div>
+      )}
+    </div> */}
       </>
     )
   }
 
   export default Quiz
+
+  // things that i still need to do:
+  // show a final score and ask to play again
+  // overall styling
+  // map the buttons:
+  //   so i can fix styling
+  //   randomize the buttons
+  // goback button
+  // the words are still messed up
+    
