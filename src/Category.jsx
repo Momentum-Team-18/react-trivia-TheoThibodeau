f5d73c94e55193e801bcaa70cf3dca3dd83663f8
TheoThibodeau <teddythibodeau@gmail.com>
2023-06-15 11:31:21 -0400

    deploy one point five

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import Quiz from './quiz'
+import Quiz from './Quiz'
 
 function Category() {
     const [categories, setCategories] = useState([])
